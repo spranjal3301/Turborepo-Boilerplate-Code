ced9c84c7db33143153b9f64ed53782f5075f6ff
spranjal394 <spranjal394@gmail.com>
2024-10-07 10:55:39 +0000

    -

--- apps/user-app/src/app/page.tsx
+++ apps/user-app/src/app/page.tsx
@@ -4,6 +4,7 @@ import {useBalance} from "@repo/store";
 
 export default function Page(): JSX.Element {
   const balance = useBalance();
+  const x=10;
   return <div>
     hi there {balance}
   </div>
